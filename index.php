838020b0797670b5be25f79c0ce421dcf5c43cda
Eudes Belmiro <eudes.bbj@gmail.com>
2021-05-20 19:22:42 -0300

    teste mac

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $simple->merge('../img/merge.jpg', 10, 25);
 $simple->crop(300, 300, 20, 40);
 $simple->setFont('../font/arial.ttf');
 $simple->setFontSize(15);
-$simple->setFontColor('#FF0000'); // ou #F00
+$simple->setFontColor('#FF0000');
 $simple->write('Test', 45, 45);
 $simple->save();
 
